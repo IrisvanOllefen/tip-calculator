94c41156b2cc7ede8d67679993ef58750862e8e1
Iris van Ollefen <irisvanollefen@gmail.com>
2021-11-17 11:46:47 +0100

    Added per person functionality

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,17 @@ function HomePage() {
 
   const [billAmount, setBillAmount] = useState('')
   const [serviceRate, setServiceRate] = useState('')
+  const [peopleAmount, setPeopleAmount] = useState('')
   const [result, setResult] = useState('')
+  const [dividedResult, setDividedResult] = useState('')
   const [total, setTotal] = useState()
+  const [dividedTotal, setDividedTotal] = useState('')
 
 
   function calculateTip(event) {
     event.preventDefault()
     
-    if (billAmount === '' || serviceRate == 0) {
+    if (billAmount === '' || serviceRate == 0 || billAmount === '') {
       alert("Please enter values")
     } else {
       result = billAmount * serviceRate
@@ -20,7 +23,16 @@ function HomePage() {
       const billAmountInt = parseInt(billAmount)
       total = resultInt + billAmountInt
       setTotal(total)
-      return result, total
+        if (peopleAmount === '' || peopleAmount <= 1) {
+          peopleAmount = 1
+        } else {
+          dividedResult = (resultInt) / peopleAmount
+          setDividedResult(dividedResult)
+          dividedTotal = (total) / peopleAmount
+          setDividedTotal(dividedTotal) 
+          return dividedResult
+        }
+      return result, dividedResult, total
     }
   }
 
@@ -33,16 +45,20 @@ function HomePage() {
         <label>How was the service?</label>
         <select name="service" className="service-input" onChange={(event) => setServiceRate(event.target.value)} value={serviceRate}>
           <option value="-">Choose an option</option>
-          <option value="0.3">30% - Outstanding</option>
-          <option value="0.2">20% - Good</option>
-          <option value="0.15">15% - OK</option>
-          <option value="0.1">10% - Bad</option>
-          <option value="0.05">5% - Terrible</option>
+          <option value="0.3">Outstanding</option>
+          <option value="0.2">Good</option>
+          <option value="0.15">OK</option>
+          <option value="0.1">Bad</option>
+          <option value="0.05">Terrible</option>
         </select>
+        <label>How much people are sharing the bill?</label>
+        <input type="number" className="people-input" onChange={(event) => setPeopleAmount(event.target.value)}/>
         <input type="submit" value="Calculate" className="calculate-button" />
       </form>
       <h2 className="result">You should tip {result ? result + ' euros' : '...'}</h2>
+      <h3 className="result-people">Per person, this would mean {dividedResult ? dividedResult + ' euros' : '...'}</h3>
       <h2 className="total">So your total would be {total ? total + ' euros' : '...'}</h2>
+      <h3 className="total-people">Per person, your total would be {dividedTotal ? dividedTotal + ' euros' : '...'}</h3>
     </div>
   )
 }
